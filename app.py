2b991109d96231997405de450c4f9eb0d64f842d
MahatoOm <omprakashmahato236@gmail.com>
2025-08-04 02:44:01 -0500

    missing line

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-app.route('/')
+@app.route('/')
 def homepage():
     return "our site is live"
 
 
 
-# if __name__ == '__main__' :
+if __name__ == '__main__' :
 
-#     app.run(host = '0.0.0.0' )
+    app.run(debug  = True)
